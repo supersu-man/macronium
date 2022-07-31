5e941888cf790faf5aec2ade9f12acede66726dd
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-07-31 15:43:34 +0530

    styles refactoring

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 var toggle = document.getElementById('themeSwitch')
-var body = document.getElementById('body')
 
 if (localStorage.getItem('dark-theme')) {
-    body.classList.remove('light-theme')
-    body.classList.add('dark-theme')
+    document.body.classList.remove('light-theme')
+    document.body.classList.add('dark-theme')
     toggle.setAttribute('checked', 'true')
 } 
 
 toggle.addEventListener('change', () => {
     if (toggle.checked) {
-        body.classList.remove('light-theme')
-        body.classList.add('dark-theme')
+        document.body.classList.remove('light-theme')
+        document.body.classList.add('dark-theme')
         localStorage.setItem('dark-theme', true)
     } else {
-        body.classList.add('light-theme')
-        body.classList.remove('dark-theme')
+        document.body.classList.add('light-theme')
+        document.body.classList.remove('dark-theme')
         localStorage.removeItem('dark-theme')
     }
 })
